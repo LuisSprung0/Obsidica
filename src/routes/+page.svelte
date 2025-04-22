4dd3d47762fb3f8b1cf85a89095113cd53741cc3
CKarma <luissprung0@gmail.com>
2025-04-21 20:01:20 -0400

    Readme + fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,12 +8,11 @@
     <meta name="keywords" content="obsidica, portfolio, design, web, obsidian, UI">
     <meta name="author" content="Luis Sprung">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./app.css">
 </svelte:head>
 
 <div class="bg-neutral-900 h-screen w-screen relative">
     <div class="flex flex-col justify-center items-center h-full w-full text-center">
-        <div id="logo" class="h-128 mb-8">
+        <div id="logo" class="h-64 sm:h-96 md:h-128 mb-8">
             <img 
                 src="/images/logo.svg" 
                 alt="Obsidica logo" 
@@ -30,7 +29,7 @@
         <p>
             <a 
                 href="https://www.linkedin.com/in/luis-sprung-544480347/"
-                class="underline underline-offset-3 hover:decoration-2 decoration-neutral-500"
+                class="underline underline-offset-3 hover:decoration-2 decoration-neutral-500 text-neutral-600"
                 target="_blank"
             >
             Made By: Luis Sprung
